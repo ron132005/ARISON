a5ab00c264b4976418b8252ad4f5a334bd16144e
ron132005 <117560981+ron132005@users.noreply.github.com>
2026-02-20 12:19:44 +0800

    Update test command message with additional info

--- funcs/test.js
+++ funcs/test.js
@@ -1,4 +1,5 @@
 // Example test.js
 module.exports = (sender_psid, callSendAPI) => {
-  callSendAPI(sender_psid, { text: "This is the test command!" });
+  callSendAPI(sender_psid, { text: "This is the test command!, There are no other available commands right now except for the AI functionality" });
 };
+

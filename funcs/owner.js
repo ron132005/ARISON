e23161da71b12286b9da477ad94b118d53e2b2e9
ron132005 <117560981+ron132005@users.noreply.github.com>
2026-02-21 00:15:34 +0800

    Update owner.js

--- funcs/owner.js
+++ funcs/owner.js
@@ -4,10 +4,10 @@ module.exports = (sender_psid, callSendAPI) => {
   // Text first
   callSendAPI(sender_psid, {
     text:
-      "Owner: Ron Funiestas\n" +
-      "Current Date: " + new Date().toLocaleDateString() + "\n" +
-      "About: This AI is named ARISON, designed to help with questions and queries.\n" +
-      "Contact Me: https://www.facebook.com/ron.funiestas/",
+      "ᴏᴡɴᴇʀ: Ron Funiestas\n" +
+      "ᴄᴜʀʀᴇɴᴛ ᴅᴀᴛᴇ: " + new Date().toLocaleDateString() + "\n" +
+      "ᴀʙᴏᴜᴛ: ARISON is an AI designed to assist with questions and inquiries, with its name standing for 𝐀utonomous, 𝐑esponsive, 𝐈ntelligent 𝐒ystems for 𝐎ptimized 𝐍etworking. It represents an advanced framework built to deliver precision, efficiency, and control in complex operational environments, enabling streamlined support and intelligent responses.\n\n" +
+      "ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ: https://www.facebook.com/ron.funiestas/",
   }).then(() => {
     // Then send GIF
     callSendAPI(sender_psid, {

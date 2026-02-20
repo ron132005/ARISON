0063af5dd14635683291e8aa58e1a7724b5f83a0
ron132005 <117560981+ron132005@users.noreply.github.com>
2026-02-20 12:28:55 +0800

    Fix GIF file path and clean up comments

--- funcs/owner.js
+++ funcs/owner.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = (sender_psid, callSendAPI) => {
-  // 1️⃣ Text message
   const textMessage = {
     text: 
       "Owner: Ron Funiestas\n" +
@@ -10,15 +9,13 @@ module.exports = (sender_psid, callSendAPI) => {
       "Contact Me: https://www.facebook.com/ron.funiestas/"
   };
 
-  // Send text first
   callSendAPI(sender_psid, textMessage, () => {
-    // 2️⃣ Then send GIF
     const gifMessage = {
       attachment: {
-        type: "image", // or "video" if it's a GIF in MP4 format
+        type: "image",
         payload: {}
       },
-      filedata: path.join(__dirname, "res", "owner.gif") // make sure the file is a real GIF
+      filedata: path.join(__dirname, "..", "res", "owner.gif")
     };
 
     callSendAPI(sender_psid, gifMessage);

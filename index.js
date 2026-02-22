cfa5176624bd573e12175f6c87ce3dbfef708b1f
ron132005 <117560981+ron132005@users.noreply.github.com>
2026-02-22 15:37:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function setMessengerProfile() {
               {
                 type: "postback",
                 title: "👤 View Owner",
-                payload: "OWNER_PAYLOAD",
+                payload: "DEVELOPER_PAYLOAD",
               },
             ],
           },
@@ -160,7 +160,7 @@ async function handleMessage(psid, text, mid) {
       text: "Pick an option:",
       quick_replies: [
         { content_type: "text", title: "HELP", payload: "HELP_PAYLOAD" },
-        { content_type: "text", title: "DEVELOPER", payload: "OWNER_PAYLOAD" },
+        { content_type: "text", title: "DEVELOPER", payload: "DEVELOPER_PAYLOAD" },
         { content_type: "text", title: "MCU", payload: "MCU_PAYLOAD" },
         { content_type: "text", title: "SONG", payload: "SONG_PAYLOAD" },
         { content_type: "text", title: "LYRICS", payload: "LYRICS_PAYLOAD" },
@@ -227,6 +227,7 @@ app.listen(PORT, () => {
   console.log(`🚀 Webhook live on ${PORT}`);
   setMessengerProfile();
 });
+
 
 
 
